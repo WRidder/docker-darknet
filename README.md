b2bed9305ea747911f961cb13ea03474bdcc44fa
Wilbert van de Ridder <wilbert.ridder@gmail.com>
2018-12-03 21:51:07 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # docker-darknet
 Darknet images with cpu and gpu support.
+
+## Building
+```bash
+$ cd gpu OR cpu
+$ ./build.sh
+```
+
+## Usage
+From the root folder of the repository, execute:
+```bash
+# Usage: $ ./detect.sh <cfg> <weights> <input-image> <output filename>
+```
+So, for example:
+```bash
+$ ./gpu/detect.sh cfg/yolov3-tiny.cfg data/yolov3-tiny.weights data/dog2.jpg predictions
+```
---
+++ cpu/detect.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Usage: $ ./detect <cfg> <weights> <input-image> <output filename>
+# Example: $ ./detect.sh cfg/yolov3-tiny.cfg data/yolov3-tiny.weights data/dog2.jpg data/predictions.jpg predictions
+
+# Check input parameters
+if [ "$#" -ne 4 ]; then
+    echo "Invalid number of parameters. Usage: $ ./detect.sh <cfg> <weights> <input-image> <output file>"
+    exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# Mount the data folder in the parent directory by default
+# If you want to enable X-server display forwarding, add: -v $HOME/.Xauthority:/home/developer/.Xauthority --net=host 
+# - This window only shows if you omit the -out parameter.
+docker run --rm -e DISPLAY=$DISPLAY -v $DIR/../data:/darknet/data --name docker-darknet-cpu docker-darknet-cpu detect $1 $2 $3 -out /darknet/data/$4
