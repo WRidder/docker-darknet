783e74e851a3180444b00ece911f2e40f0d7a83a
Wilbert van de Ridder <wilbert.ridder@gmail.com>
2018-12-04 19:44:04 +0100

    Fix volume path.

--- detect.sh
+++ detect.sh
@@ -13,4 +13,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # Mount the data folder in the parent directory by default
 # If you want to enable X-server display forwarding, add: -v $HOME/.Xauthority:/home/developer/.Xauthority --net=host 
 # - This window only shows if you omit the -out parameter.
-docker run --runtime=nvidia --rm -e DISPLAY=$DISPLAY -v $DIR/../data:/darknet/data --name docker-darknet-gpu wridder/docker-darknet:gpu detect $1 $2 $3 -out /darknet/data/$4
+docker run --runtime=nvidia --rm -e DISPLAY=$DISPLAY -v $DIR/data:/darknet/data --name docker-darknet-gpu wridder/docker-darknet:gpu detect $1 $2 $3 -out /darknet/data/$4
