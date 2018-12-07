9ca158726660851b12570666220600e844bcea14
Wilbert van de Ridder <wilbert.ridder@gmail.com>
2018-12-07 08:50:05 +0100

    Added switch to choose gpu/cpu inference

--- README.md
+++ README.md
@@ -9,12 +9,15 @@ $ cd docker-darknet
 ```
 From the root folder of the repository, execute:
 ```bash
-# Usage: $ ./detect.sh <cfg> <weights> <input-image> <output filename>
+# Usage: $ ./detect.sh <cfg> <weights> <input-image> <output filename> [<use-gpu>=0/1]
 ```
 So, for example:
 ```bash
 $ ./detect.sh cfg/yolov3-tiny.cfg data/yolov3-tiny.weights data/dog2.jpg predictions
 ```
+
+The image mounts the *data* folder by default. Any weights, configuration, input images, etc can be located. By default, a nVidia CUDA compataible GPU is assumed to be available. 
+
 
 ## Building
 ```bash
--- detect.sh
+++ detect.sh
@@ -2,15 +2,27 @@
 # Usage: $ ./detect <cfg> <weights> <input-image> <output filename>
 # Example: $ ./detect.sh cfg/yolov3-tiny.cfg data/yolov3-tiny.weights data/dog2.jpg data/predictions.jpg predictions
 
+# Variables
+USE_GPU=1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
 # Check input parameters
-if [ "$#" -ne 4 ]; then
-    echo "Invalid number of parameters. Usage: $ ./detect.sh <cfg> <weights> <input-image> <output file>"
+if [ "$#" -lt 4 ]; then
+    echo "Invalid number of parameters. Usage: $ ./detect.sh <cfg> <weights> <input-image> <output file> [<use-gpu>=0/1]"
     exit 1
 fi
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+if [ "$#" -gt 4 ]; then
+    if [ "$5" -ne 1 ]; then
+        USE_GPU=0
+    fi
+fi
 
 # Mount the data folder in the parent directory by default
 # If you want to enable X-server display forwarding, add: -v $HOME/.Xauthority:/home/developer/.Xauthority --net=host 
 # - This window only shows if you omit the -out parameter.
-docker run --runtime=nvidia --rm -e DISPLAY=$DISPLAY -v $DIR/data:/darknet/data --name docker-darknet-gpu wridder/docker-darknet:gpu detect $1 $2 $3 -out /darknet/data/$4
+if [ "$USE_GPU" -eq 1 ]; then
+    docker run --runtime=nvidia --rm -e DISPLAY=$DISPLAY -v $DIR/data:/darknet/data --name docker-darknet-gpu wridder/docker-darknet:gpu detect $1 $2 $3 -out /darknet/data/$4
+else
+    docker run --rm -e DISPLAY=$DISPLAY -v $DIR/data:/darknet/data --name docker-darknet-cpu wridder/docker-darknet:cpu detect $1 $2 $3 -out /darknet/data/$4
+fi  
